af6a43b7881948b1b93629c3cd8e690b03b9e511
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-05-01 15:38:10 +0900

    20210501153740-add tools

--- setting/vs_setting.bat
+++ setting/vs_setting.bat
@@ -4,8 +4,3 @@ rem     Released under the MIT license
 rem     https://opensource.org/licenses/mit-license.php
 set PathToVsDevCmd="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 set PathToVcVars="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-if not "%VS140COMNTOOLS%" == "" (
-   set PathToVsDevCmd="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-   set PathToVcVars=
-)

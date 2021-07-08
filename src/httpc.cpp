44743c87d108d3ea1b54c09f605d4f2a3b580a29
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-07-08 19:35:14 +0900

    20210708193513-edit

--- src/httpc.cpp
+++ src/httpc.cpp
@@ -6,16 +6,24 @@
 
 int main(int argc,char** argv){
     //setlocale(LC_ALL,".UTF-8");
+
+#ifdef _WIN32
     WSAData data;
     if(WSAStartup(MAKEWORD(2,2),&data)){
         std::cout << "wsa init failed.";
         return -1;
     }
+#endif
     SSL_load_error_strings();
-    auto ret=netclient_str(GetCommandLineA());
+    int ret=0;
+#ifdef _WIN32
+    ret=netclient_str(GetCommandLineA());
+#endif
     commonlib2::ArgChange _(argc,argv);
     ret=command_argv(argc,argv);
     //http2_test();
+#ifdef _WIN32
     WSACleanup();
+#endif
     return ret;
 }
